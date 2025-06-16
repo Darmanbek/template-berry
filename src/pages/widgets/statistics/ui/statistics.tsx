4bb9f1bfffb2b82e98b0f9e252279c7e4965f316
Darmanbek <pahratdinovdarman@gmail.com>
2025-06-16 17:41:57 +0500

    Fix Errors

--- src/pages/widgets/statistics/ui/statistics.tsx
+++ src/pages/widgets/statistics/ui/statistics.tsx
@@ -1,26 +1,26 @@
-import { PieChartOutlined } from "@ant-design/icons";
-import { FC } from "react";
-import { UiPageHeader } from "src/components/ui";
+// import { PieChartOutlined } from "@ant-design/icons";
+import type { FC } from "react"
+// import { UiPageHeader } from "src/shared/ui";
 
 const Statistics: FC = () => {
 	return (
 		<>
-			<UiPageHeader
-				title={"Statistics"}
-				paths={[
-					{
-						path: "/widget/statistics",
-						title: (
-							<>
-								<PieChartOutlined />
-								<span>Statistics</span>
-							</>
-						),
-					},
-				]}
-			/>
+			{/*<UiPageHeader*/}
+			{/*	title={"Statistics"}*/}
+			{/*	paths={[*/}
+			{/*		{*/}
+			{/*			path: "/widget/statistics",*/}
+			{/*			title: (*/}
+			{/*				<>*/}
+			{/*					<PieChartOutlined />*/}
+			{/*					<span>Statistics</span>*/}
+			{/*				</>*/}
+			{/*			),*/}
+			{/*		},*/}
+			{/*	]}*/}
+			{/*/>*/}
 		</>
-	);
-};
+	)
+}
 
-export default Statistics;
+export default Statistics
